d77583c43b8a0950f9087cd007d6a49ceab673d7
YUANZHI-WONG <65759514+YUANZHI-WONG@users.noreply.github.com>
2020-10-15 22:53:44 +0800

    Update catch.py

--- catch.py
+++ catch.py
@@ -8,13 +8,28 @@ import random
 
 
 
+my_headers = [
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)",
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    'Opera/9.25 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 "]
+
 
 headers = {
-    'Cookie': 'appver=1.5.0.7573471;',
+    'Cookie': 'appver=1.5.0.75771;',
     'Host':'music.163.com',
     'Origin':'https://music.163.com',
     'Referer':'https://music.163.com/song?id=374597',
-    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+    'User-Agent':str(random.choice(my_headers))
 }
 
 first_param = '{rid:"", offset:"20", total:"false", limit:"50", csrf_token:""}'
@@ -60,6 +75,9 @@ def get_json(url, params, encSecKey):
          "params": params,
          "encSecKey": encSecKey
     }
+
+    #headers = str(random.choice(my_headers))
+    #headers = str(headers)
     response = requests.post(url, headers=headers, data=data)
     return response.content
 
