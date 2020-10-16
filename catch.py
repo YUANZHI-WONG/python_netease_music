8820bbe5409524522f2713acf22f14e87061c64a
YUANZHI-WONG <65759514+YUANZHI-WONG@users.noreply.github.com>
2020-10-16 22:00:39 +0800

    wx

--- catch.py
+++ catch.py
@@ -7,26 +7,15 @@ import time
 import random
 
 
-<<<<<<< HEAD
-
-
-
-headers = {
-
-    'content-type':'application/x-www-form-urlencoded'
-    'cookie':'_iuqxldmzr_=32; _ntes_nnid=30d98f25eb6c5f49c33c6afcbf0bcb8a,1592997711464; _ntes_nuid=30d98f25eb6c5f49c33c6afcbf0bcb8a; WM_TID=qecawFKf3t9FEUUBUVY%2FHbw9Q1QSbLk4; ntes_kaola_ad=1; mail_psc_fingerprint=50abad436a9db9dbce7970da6751f786; vinfo_n_f_l_n3=1acba15b97f98f69.1.0.1597198752056.0.1597198767601; NMTID=00Opi5KUfSfNaLFnUzxoWAP1GWbCfgAAAF1G_ctug; P_INFO=wangxu5789@126.com|1602659300|0|mail126|00&99|bej&1602659297&mail_client#bej&null#10#0#0|&0|yanxuan&mailmaster_android&mail126_qrcode|wangxu5789@126.com; JSESSIONID-WYYY=my%2BNqUCflpn6%2B5%5CP4yN%2Bc%2B%2BI0GI6dy9wzzKozMMGldigvbvUWPQjBzHB2UXaZdNYu9lR31AfDm7aH%2BWbOdOyQTtAJRWyBijIJWmC2wRg7NTUEmbPFdKC8XvYKOM2nPppmPM404pobN0EwTXVRdoZkJwWVzutoNTjpwYZ563J4XSmn8j0%3A1602857658354; WM_NI=TEZEDOSRjedOjaAw8qY8Nnowswpq4%2FvVtsnSDHd%2F4e71DUvlg9tiD6ubyvlYBmLjH31t3eO1Gc5IIdTeJMy7YzIleB44NGgVj19E1oQL1nsZ%2F95OSI5wqjd5ujnxAUnENFo%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6eeaefc5f97f0a9b3ca66fc8a8ea7d14f828b9b84f470bcb08d8fd265edb589d5cb2af0fea7c3b92a989b96a6c745918ea686e745aabf8f98bb7b9be8828fee7db1a9c092b725b68abc94ed598898acd0b76ff5ef8cb6f568a5e7bc8ac73eb7beb797ef5382b3848bcb39f3acfd86ce3b85bdfbaacc7098bb86aefc3d9ba6878de84af8ec82a8e65eaaeff7a6b4659394ff8ff252ede7fbb0d243f7a9bf8be73fb7b6a299c45fae9b82a7ee37e2a3'
-    'Host':'music.163.com',
-    'Origin':'https://music.163.com',
-    'Referer':'https://music.163.com/song?id=1421256202',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36 Edg/86.0.622.38'
-=======
 headers = {
     'Cookie': 'appver=1.5.0.75771;',
-    'Referer': 'http://music.163.com/'
->>>>>>> parent of 7b92964... Update catch.py
+    'Host':'music.163.com',
+    'Origin':'https://music.163.com',
+    'Referer':'https://music.163.com/song?id=374597',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36 Edg/86.0.622.38'   
 }
 
-first_param = '{rid:"", offset:"20", total:"false", limit:"50", csrf_token:""}'
+#first_param = '{rid:"", offset:"20", total:"false", limit:"50", csrf_token:""}'
 #offset就是（评论页数-1） * 20，total在第一页是true，其余是false
 
 second_param = "010001"
@@ -88,7 +77,6 @@ def get_all_comments(url,page):
             for item in json_dict['comments']:
                 comment = item['content'] # 评论内容
                 comment_info = str(comment)
-                print(comment)
                 all_comments_list.append(comment_info)
         except:
             print(json_dict)
