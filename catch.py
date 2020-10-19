5c29facd3d30a9aedc374dbddb5f4892a1cadf64
YUANZHI-WONG <65759514+YUANZHI-WONG@users.noreply.github.com>
2020-10-19 12:43:26 +0800

    wx

--- catch.py
+++ catch.py
@@ -31,21 +31,23 @@ forth_param = "0CoJUm6Qyw8W8jud"
 
 #获取参数
 #    page: 爬取的页码 
-def get_params(page):                               
+def get_params(page):  
     iv = b"0102030405060708"                        #引申学习 ——> python字符串前缀u，b，r，f用法
     first_key = forth_param
     second_key = 16 * 'F'
     if(page == 1): # 如果为第一页
-        first_param = '{rid:"", offset:"0", total:"true", limit:"20", csrf_token:""}'
+        first_param = '{rid:"", offset:"0", total:"true", limit:"20", csrf_token:""}'   
                                                     #offset就是（评论页数-1） * 20，total在第一页是true，其余是false
         h_encText = AES_encrypt(first_param, first_key, iv)
     else:
         offset = str((page-1)*20)
-        first_param = '{rid:"", offset:"%s", total:"%s", limit:"20", csrf_token:""}' %(offset,'false')
+        first_param = '{rid:"", offset:"%s", total:"%s", limit:"20", csrf_token:""}' %(offset,'false')  
                                                      #学习Python格式化拼接的方法，%(offset,'false')
         h_encText = AES_encrypt(first_param, first_key, iv)
-    h_encText = AES_encrypt(h_encText, second_key, iv)#调用AES分组加密算法函数，获得加密后的内容
+    h_encText = AES_encrypt(h_encText, second_key, iv)#调用AES分组加密算法函数，获得加密后的内容   AES  RSA  哈希函数。 国密算法 SM4  SM2  SM3   
     return h_encText
+###########################################################################
+
 
 #获取密钥
 def get_encSecKey():
@@ -60,9 +62,9 @@ def AES_encrypt(text, key, iv):
     pad = 16 - len(text) % 16          #len()函数的作用：？
     text = text + pad * chr(pad)       #chr()函数的作用：？
     text=text.encode("utf-8")            #类型转换：str to bytes 
-    encryptor = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv)
+    encryptor = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv)  
     encrypt_text = encryptor.encrypt(text)
-    encrypt_text = base64.b64encode(encrypt_text)
+    encrypt_text = base64.b64encode(encrypt_text) 
     return encrypt_text.decode('utf-8')  #类型转换：bytes to str
                                          # bytes（一堆二进制的数字，如：b'11001010'）主要是给在计算机看的，string主要是给人看的
 
@@ -92,24 +94,28 @@ def get_all_comments(url,page_o,page_d):
         params = get_params(i)#变动的
         encSecKey = get_encSecKey()#固定的
 
-        json_text = get_json(url,params,encSecKey)
+        json_text = get_json(url,params,encSecKey) ########
 
         json_dict = json.loads(json_text) #JSON格式的字符串处理办法      
         try:   
-            for item in json_dict['comments']:
-                comment = item['content']                #评论内容
-                comment_info = str(comment)              #str()函数的作用：？
-                all_comments_list.append(comment_info)   #list.append() 给 列表型变量追加 内容
-
-            
+            for item in json_dict['hotComments']:    #热评的格式还需要重新解析
+             #for item in json_dict['comments']:
+                try:
+                    comment = item['content']                #评论内容
+                    comment_info = str(comment)              #str()函数的作用：？
+                    all_comments_list.append(comment_info)   #list.append() 给 列表型变量追加 内容
+                except:
+                    print(item)             
         except:
-            print(json_dict)                             #try：  except: 的用法？
+            print('\n')                             #try：  except: 的用法？
         
         print('第%d页抓取完毕!' %(i))                    #格式化屏幕打印的写法 print('  %变量类型简写  ' %(变量名))
         
         #sleep_time=random.choice(range(6,15))             #从6到15之间取个随机数，作为休眠时间
         sleep_time=random.randrange(6,15)
-        print(f"休眠:{sleep_time}s")                      #复习 python字符串前缀u，b，r，f用法
+
+        print(f"休眠:{sleep_time}s") 
+        #print('休眠:%d s'  %(sleep_time))                 #复习 python字符串前缀u，b，r，f用法
                                                           # python的单引号 '' 和双引号 ""  没区别,可以混用  
         time.sleep(sleep_time)                            #设置休眠时间，跑慢点，减轻服务器负担，避免触发反爬机制
     return all_comments_list
@@ -118,13 +124,13 @@ def get_all_comments(url,page_o,page_d):
 if __name__ == "__main__":
     url = "http://music.163.com/weapi/v1/resource/comments/R_SO_4_1297747234/?csrf_token="   #  R_SO_4_xxxxxxx加上歌曲的id就是抓取评论的API接口
     
-    all_comments = get_all_comments(url, page_o=1,page_d=7)          # 需要爬取的页面范围    
+    all_comments = get_all_comments(url, page_o=1,page_d=3)          # 需要爬取的页面范围    
     
     print(all_comments)                                              #打印爬到的内容
     #同时遍历两个列表
 
-    #file_tag=time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())    #将当前时间作为输出文本文件名
-    file_tag='All Night Long'                                                 #将歌曲名为输出文本文件名
+    file_tag=time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())    #将当前时间作为输出文本文件名
+    #file_tag='All Night Long'                                                 #将歌曲名为输出文本文件名
 
     save_path=f'all_comments_{file_tag}.txt'                         #文件保存路径
 
---
+++ simple.py
@@ -0,0 +1,4 @@
+#课后练习
+#这是一个没有加密的url··················
+
+http://music.163.com/api/v1/resource/comments/R_SO_4_516997458
